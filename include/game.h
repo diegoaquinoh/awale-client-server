3b9291ac3d499f176e2c58e5f3106f9d38ef2473
diegoaquinoh <diegoaquinoh@gmail.com>
2025-10-21 09:28:06 +0200

    more function declarations

--- include/game.h
+++ include/game.h
@@ -10,6 +10,8 @@
 //---------- Interface of the <Game> (file Game.h) ----------------
 #if ! defined ( Game_H )
 #define Game_H
+#include <stdio.h>
+#include <stdlib.h>
 
 //--------------------------------------------------- Interfaces used
 using namespace std;
@@ -27,5 +29,14 @@ char board[12];
 char collected = 0;
 
 void init_game();
+
+void display_board();
+char is_game_over();
+void collect_remaining_seeds();
+void declare_winner();
+
+void play_turn(char player);
+void move(char player, char pit_index);
+
 
 #endif // Game_H
