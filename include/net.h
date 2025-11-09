2b4823da2acc73225ab5d6c9081dfdf48ddd26be
Lemseffer Mohamed <mlemseffer@2D-LINUX-053.insa-lyon.fr>
2025-11-09 13:42:31 +0100

    mise en forme du code

--- include/net.h
+++ include/net.h
@@ -1,3 +1,14 @@
+/*************************************************************************
+                           Awale -- Game
+                             -------------------
+    début                : 21/10/2025
+    copyright            : (C) 2025 par Mohamed et Diego
+    e-mail               : mohamed.lemseffer@insa-lyon.fr / diego.aquinoh@insa-lyon.fr / 
+
+*************************************************************************/
+
+//---------- Interface of the <net> (file net.h) ----------------
+
 #ifndef NET_H
 #define NET_H
 #include "game.h"
